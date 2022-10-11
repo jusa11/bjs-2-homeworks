d669b22efac25eeab7a1a963e0a227fa82399151
Arman Dzhusupov <jusa11@yandex.ru>
2022-10-11 23:20:08 +0600

    tasks completed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,78 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+	let min, max, sum, avg;
+	min = Infinity;
+	max = -Infinity;
+	sum = 0;
 
-  // Ваш код
+	// поиск минимального и максимального элемента в массиве
+	for (let i = 0; i < arr.length; i++) {
+		sum = arr[i] + sum;
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
 
-  return { min: min, max: max, avg: avg };
+	// расчет среднего арифметического значения
+	avg = +(sum / arr.length).toFixed(2);
+
+	return { min: min, max: max, avg: avg };
 }
+
 
 // Задание 2
+
+/* насадка */
 function worker(arr) {
-  let sum;
+	let sum = 0;
 
-  // Ваш код
-
-  return sum;
+	// расчет суммы всех элементов в массиве
+	for (let i = 0; i < arr.length; i++) {
+		sum = arr[i] + sum;
+	}
+	return sum;
 }
 
+/* мясорубка */
 function makeWork(arrOfArr, func) {
-  let max;
+	let max = 0;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+	// поиск "самого жирного" куска мяса 
+	for (let i = 0; i < arrOfArr.length; i++) {
+		if (func(arrOfArr[i]) > max) {
+			max = func(arrOfArr[i])
+		}
+	}
+	return max;
 }
+
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+	let sumMax = 0;
+	let sumMin = 0;
+	let difference;
+
+	// поиск максимального значения
+	for (let i = 0; i < arr.length; i++) {
+		sumMax = arr[0];
+		if (arr[i] > sumMax) {
+			sumMax = arr[i];
+		}
+	}
+
+	// поиск минимального значения
+	for (let j = 0; j < arr.length; j++) {
+		sumMin = arr[0];
+		if (arr[j] < sumMin) {
+			sumMin = arr[j];
+		}
+	}
+
+	// разница значений
+	difference = Math.abs(sumMax - sumMin);
+	return difference;
 }
