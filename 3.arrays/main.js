40f4c50bc3469f930da042aac83478c416947fe0
Arman Dzhusupov <jusa11@yandex.ru>
2022-10-17 21:56:18 +0600

    completed tasks on arrays

--- 3.arrays/main.js
+++ 3.arrays/main.js
@@ -1,6 +1,14 @@
 console.log(compareArrays([2, 2, 2], [2, 2, 2]))
 console.log(compareArrays([1], [1, 2, 3]))
 console.log(compareArrays([1, 2, 3], [1]))
+console.log(compareArrays([8, 9], [6]))// false, разные значения
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5])) // false, разные значения
+console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4])) // false, разные значения
+console.log(compareArrays([1, 2, 3], [2, 3, 1])) // false, разные индексы, хотя и одинаковые значения
+console.log(compareArrays([8, 1, 2], [8, 1, 2])) // true
+
+
+
 console.log(advancedFilter([-10, 12, -12, -3]))
 console.log(advancedFilter([-6, 6, -21, 21]))
 console.log(advancedFilter([0, 11, -0, 12]))
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+	let result;
+	if (arr1.length === arr2.length) {
+		result = arr1.every((item, index) => item === arr2[index])
+	}
+	else {
+		return false
+	}
+	return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+	return resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number =>
+		number * 10); // array
 }
