9cf1a68cd9af2b99cc70d0caaa7fddb8b69f08b8
Arman Dzhusupov <jusa11@yandex.ru>
2022-10-29 21:45:32 +0600

    there are problems with the 3rd task

--- 5.classes/main.js
+++ 5.classes/main.js
@@ -27,3 +27,34 @@ console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
 console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
 library.giveBookByName("Машина времени");
 console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+
+/* Задача № 3 */
+const student = new Student("Олег Никифоров", 'man', 25);
+console.log(student)
+student.addMark(5, "algebra");
+student.addMark(2, "algebra");
+student.addMark(5, "geometry");
+student.addMark(4, "geometry");
+student.addMark(3, "history");
+student.addMark(4, "history");
+student.addMark(2, "geography");
+student.addMark(7, "geography"); // "Ошибка, оценка должна быть числом от 1 до 5"
+student.getAverageBySubject("geometry"); // Средний балл по предмету geometry 4.5
+student.getAverageBySubject("algebra");
+student.getAverageBySubject("history");
+student.getAverageBySubject("geography");
+student.getAverageBySubject("biology"); // Несуществующий предмет
+student.getAverage(); // Средний балл по всем предметам 
+
+
+const student2 = new Student("Иван Иванов", 'man', 24);
+console.log(student2)
+student2.addMark(5, "algebra");
+student2.addMark(5, "algebra");
+student2.addMark(5, "geometry");
+student2.addMark(4, "geometry");
+student2.addMark(6, "geometry"); // "Ошибка, оценка должна быть числом от 1 до 5"
+student2.getAverageBySubject("geometry"); // Средний балл по предмету geometry 4.5
+student2.getAverageBySubject("biology"); // Несуществующий предмет
+student2.getAverage(); // Средний балл по всем предметам 4.75
+student2.exclude("Исключен за попытку подделать оценки");
--- 5.classes/task.js
+++ 5.classes/task.js
@@ -30,65 +30,41 @@ class PrintEditionItem {
 
 class Magazine extends PrintEditionItem {
 	constructor(name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount)
-		this.name = name;
-		this.releaseDate = releaseDate;
-		this.pagesCount = pagesCount;
-		this.state = 100;
+		super(name, releaseDate, pagesCount);
 		this.type = "magazine";
 	}
 }
 
 class Book extends PrintEditionItem {
 	constructor(author, name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount)
+		super(name, releaseDate, pagesCount);
 		this.author = author;
-		this.name = name;
-		this.releaseDate = releaseDate;
-		this.pagesCount = pagesCount;
-		this.state = 100;
 		this.type = "book";
 	}
 }
 
 class NovelBook extends Book {
 	constructor(author, name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount)
-		this.author = author;
-		this.name = name;
-		this.releaseDate = releaseDate;
-		this.pagesCount = pagesCount;
-		this.state = 100;
+		super(author, name, releaseDate, pagesCount);
 		this.type = "novel";
 	}
 }
 
 class FantasticBook extends Book {
 	constructor(author, name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount)
-		this.author = author;
-		this.name = name;
-		this.releaseDate = releaseDate;
-		this.pagesCount = pagesCount;
-		this.state = 100;
+		super(author, name, releaseDate, pagesCount);
 		this.type = "fantastic";
 	}
 }
 
 class DetectiveBook extends Book {
 	constructor(author, name, releaseDate, pagesCount) {
-		super(name, releaseDate, pagesCount)
-		this.author = author;
-		this.name = name;
-		this.releaseDate = releaseDate;
-		this.pagesCount = pagesCount;
-		this.state = 100;
+		super(author, name, releaseDate, pagesCount);
 		this.type = "detective";
 	}
 }
 
 /* Задача № 2 */
-
 class Library {
 	constructor(name, books) {
 		this.name = name;
@@ -117,4 +93,86 @@ class Library {
 
 };
 
+
+/* 3 задача */
+class Subjects {
+	constructor(name) {
+		this.name = name;
+		this.mark = [];
+		this.averageMark = null;
+	}
+}
+
+class Student {
+	constructor(name, gender, age) {
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.Subjects = [];
+		this.totalAverageMarks = 0;
+	}
+
+	// добавление предметов
+	addSubjects(subject) {
+		this.Subjects.push(subject);
+	}
+
+	// добавление оценок
+	addMark(mark, subjectName) {
+		let Subject = this.Subjects.find((Subject) => Subject.name === subjectName);
+		if (Subject) {
+			if (mark < 1 || mark > 5) {
+				console.log('Ошибка: Неверное значение выставляемой оценки. Оценка должна быть от 1 до 5');
+			}
+			else {
+				Subject.mark.push(mark);
+			}
+		}
+		// Если такого предмета нет в журнале, то добавить его и выставить оценку
+		else {
+			this.addSubjects(new Subjects(subjectName));
+			let Subject = this.Subjects.find((Subject) => Subject.name === subjectName);
+			Subject.mark.push(mark);
+		}
+		return Subject;
+	}
+
+	// расчет среднего арифметического оценок студента
+	getAverageBySubject(subjectName) {
+		let Average;
+		let Subject = this.Subjects.find((Subject) => Subject.name === subjectName);
+		if (Subject) {
+			Subject.mark.reduce((acc, item, index, arr) => {
+				acc += item;
+				if (index === arr.length - 1) {
+					Average = acc / arr.length;
+					Subject.averageMark = Average;
+				}
+				return acc;
+			}, 0)
+		}
+		else {
+			console.log('Несуществующий предмет');
+		}
+		return Average;
+	}
+
+	// расчет подсчёт общей средней оценки
+	getAverage() {
+		let result;
+		let acc = 0;
+		for (let i = 0; i < this.Subjects.length; i++) {
+			acc = this.Subjects[i].averageMark + acc;
+		}
+		result = acc / this.Subjects.length;
+		this.totalAverageMarks = +result.toFixed(2);
+		return this.totalAverageMarks
+	}
+
+	// отчисление студента
+	exclude(reason) {
+		delete this.Subjects;
+		this.excluded = reason;
+	}
+}
 
