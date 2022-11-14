1f54a3e454a28327da04f87fda1c3af45383f612
Arman Dzhusupov <jusa11@yandex.ru>
2022-11-14 23:43:26 +0600

    completed the task

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,45 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = args.join(',');
+		const objCache = cache.find((item) => item.hash === hash);
+
+		if (objCache) {
+			console.log("Из кэша: " + objCache.result);
+			return "Из кэша: " + objCache.result;
+		}
+
+		const result = func(...args);
+		cache.push({ hash, result });
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем:  " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+
+function debounceDecoratorNew(func, delay) {
+	let timeoutId = null;
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	function wrapper(...args) {
+		wrapper.allCount++; 
+		if (!timeoutId) {
+			func(...args);
+			wrapper.count++;
+		}
+		clearTimeout(timeoutId);
+	
+		timeoutId = setTimeout(() => {
+			wrapper.count++;
+			func(...args);
+		}, delay);
+	}
+	return wrapper;
 }
